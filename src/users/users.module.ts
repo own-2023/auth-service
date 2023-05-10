983fd4e2eb485745749d3428f51ed6bb2c3d85b2
Halil Uzer <haliluzer@outlook.com>
2023-05-10 16:46:24 +0300

    temporal removed

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,7 +4,7 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { UserRepository } from './repositories/user.repository';
-import { HttpModule, HttpService } from '@nestjs/axios/dist';
+import { HttpModule } from '@nestjs/axios/dist';
 @Module({
     imports:[TypeOrmModule.forFeature([User]), HttpModule],
     providers:[UsersService, UserRepository],
